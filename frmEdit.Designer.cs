31d712cf545150fb42c4ffa4c158d5e9a322c057
arblackmon1 <55294712+arblackmon1@users.noreply.github.com>
2019-12-12 19:35:42 -0600

    final

---
+++ frmEdit.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace PA6_code
+{
+    partial class frmEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.lblISBN = new System.Windows.Forms.Label();
+            this.txtCopiesData = new System.Windows.Forms.TextBox();
+            this.lblCopies = new System.Windows.Forms.Label();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.lblGenre = new System.Windows.Forms.Label();
+            this.txtAuthorData = new System.Windows.Forms.TextBox();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.txtTitleData = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtCover = new System.Windows.Forms.TextBox();
+            this.lblCover = new System.Windows.Forms.Label();
+            this.pbCover = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(10, 326);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(157, 29);
+            this.txtLength.TabIndex = 23;
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(8, 299);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(68, 24);
+            this.lblLength.TabIndex = 22;
+            this.lblLength.Text = "Length";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(10, 267);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(157, 29);
+            this.txtISBN.TabIndex = 21;
+            // 
+            // lblISBN
+            // 
+            this.lblISBN.AutoSize = true;
+            this.lblISBN.Location = new System.Drawing.Point(8, 240);
+            this.lblISBN.Name = "lblISBN";
+            this.lblISBN.Size = new System.Drawing.Size(52, 24);
+            this.lblISBN.TabIndex = 20;
+            this.lblISBN.Text = "ISBN";
+            // 
+            // txtCopiesData
+            // 
+            this.txtCopiesData.Location = new System.Drawing.Point(12, 208);
+            this.txtCopiesData.Name = "txtCopiesData";
+            this.txtCopiesData.Size = new System.Drawing.Size(157, 29);
+            this.txtCopiesData.TabIndex = 19;
+            // 
+            // lblCopies
+            // 
+            this.lblCopies.AutoSize = true;
+            this.lblCopies.Location = new System.Drawing.Point(11, 181);
+            this.lblCopies.Name = "lblCopies";
+            this.lblCopies.Size = new System.Drawing.Size(150, 24);
+            this.lblCopies.TabIndex = 18;
+            this.lblCopies.Text = "Copies Available";
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(12, 149);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(157, 29);
+            this.txtGenre.TabIndex = 17;
+            // 
+            // lblGenre
+            // 
+            this.lblGenre.AutoSize = true;
+            this.lblGenre.Location = new System.Drawing.Point(11, 122);
+            this.lblGenre.Name = "lblGenre";
+            this.lblGenre.Size = new System.Drawing.Size(63, 24);
+            this.lblGenre.TabIndex = 16;
+            this.lblGenre.Text = "Genre";
+            // 
+            // txtAuthorData
+            // 
+            this.txtAuthorData.Location = new System.Drawing.Point(12, 90);
+            this.txtAuthorData.Name = "txtAuthorData";
+            this.txtAuthorData.Size = new System.Drawing.Size(157, 29);
+            this.txtAuthorData.TabIndex = 15;
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(8, 63);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(66, 24);
+            this.lblAuthor.TabIndex = 14;
+            this.lblAuthor.Text = "Author";
+            // 
+            // txtTitleData
+            // 
+            this.txtTitleData.Location = new System.Drawing.Point(10, 31);
+            this.txtTitleData.Name = "txtTitleData";
+            this.txtTitleData.Size = new System.Drawing.Size(157, 29);
+            this.txtTitleData.TabIndex = 13;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(6, 4);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(45, 24);
+            this.lblTitle.TabIndex = 12;
+            this.lblTitle.Text = "Title";
+            // 
+            // txtCover
+            // 
+            this.txtCover.Location = new System.Drawing.Point(14, 385);
+            this.txtCover.Name = "txtCover";
+            this.txtCover.Size = new System.Drawing.Size(157, 29);
+            this.txtCover.TabIndex = 25;
+            // 
+            // lblCover
+            // 
+            this.lblCover.AutoSize = true;
+            this.lblCover.Location = new System.Drawing.Point(12, 358);
+            this.lblCover.Name = "lblCover";
+            this.lblCover.Size = new System.Drawing.Size(101, 24);
+            this.lblCover.TabIndex = 24;
+            this.lblCover.Text = "Cover URL";
+            // 
+            // pbCover
+            // 
+            this.pbCover.Location = new System.Drawing.Point(336, 31);
+            this.pbCover.Name = "pbCover";
+            this.pbCover.Size = new System.Drawing.Size(189, 324);
+            this.pbCover.TabIndex = 26;
+            this.pbCover.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(336, 397);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(435, 397);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 36);
+            this.btnClose.TabIndex = 28;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 488);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pbCover);
+            this.Controls.Add(this.txtCover);
+            this.Controls.Add(this.lblCover);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.lblISBN);
+            this.Controls.Add(this.txtCopiesData);
+            this.Controls.Add(this.lblCopies);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.lblGenre);
+            this.Controls.Add(this.txtAuthorData);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.txtTitleData);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "frmEdit";
+            this.Text = "frmEdit";
+            this.Load += new System.EventHandler(this.frmEdit_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCover)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label lblISBN;
+        private System.Windows.Forms.TextBox txtCopiesData;
+        private System.Windows.Forms.Label lblCopies;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.Label lblGenre;
+        private System.Windows.Forms.TextBox txtAuthorData;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.TextBox txtTitleData;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtCover;
+        private System.Windows.Forms.Label lblCover;
+        private System.Windows.Forms.PictureBox pbCover;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ frmEdit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA6_code
+{
+    public partial class frmEdit : Form
+    {
+        private Book myBook;
+        private string cwid;
+        private string mode;
+
+        public frmEdit(Object tempBook, string tempMode, string tempCwid)
+        {
+            myBook = (Book)tempBook;
+            cwid = tempCwid;
+            mode = tempMode;
+            InitializeComponent();
+            pbCover.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void frmEdit_Load(object sender, EventArgs e)
+        {
+            if(mode == "edit")
+            {
+                txtTitleData.Text = myBook.title;
+                txtAuthorData.Text = myBook.author;
+                txtGenre.Text = myBook.genre;
+                txtCopiesData.Text = myBook.copies.ToString();
+                txtISBN.Text = myBook.isbn;
+                txtCover.Text = myBook.cover;
+                txtLength.Text = myBook.length.ToString();
+
+                pbCover.Load(myBook.cover);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+
+            this.Close();
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            myBook.title = txtTitleData.Text;
+            myBook.author = txtAuthorData.Text;
+            myBook.genre = txtGenre.Text;
+            myBook.copies = int.Parse(txtCopiesData.Text);
+            myBook.isbn = txtISBN.Text;
+            myBook.cover = txtCover.Text;
+            myBook.length = int.Parse(txtLength.Text);
+
+            BookFile.SaveBook(myBook, cwid, mode);
+            MessageBox.Show("Content was saved.", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
